ef3d674d9d1b0c061e9c59c4987ebb681c62898b
bergloman <bergloman@gmail.com>
2016-08-23 06:29:15 +0200

    Added utils, improved minifications

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,4 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
   // Project configuration.
   grunt.initConfig({
@@ -8,16 +8,30 @@ module.exports = function(grunt) {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
       build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
+        files: [{
+          'build/<%= pkg.name %>.min.js': ['src/<%= pkg.name %>.js'],
+          'build/<%= pkg.name %>-utils.min.js': ['src/<%= pkg.name %>-utils.js']
+        }]
+      }
+    }, 
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'src',
+          src: ['*.css'],
+          dest: 'build',
+          ext: '.min.css'
+        }]
       }
     }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['uglify', "cssmin"]);
 
 };
