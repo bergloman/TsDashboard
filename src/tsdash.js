bc2540b71b93eaada85e82ce62f11d4052a591ed
Viktor Jovanoski <viktor@carvic.si>
2016-10-14 10:45:07 +0200

    removed dangerous polyfills, fixed array iteration

--- src/tsdash.js
+++ src/tsdash.js
@@ -74,7 +74,7 @@ TsDashboard.prototype.getParamValue = function (name) {
 
 TsDashboard.prototype.setParamValue = function (name, value) {
     var self = this;
-    for (var ii in self.conf.parameters) (function (i) {
+    for (var ii = 0; ii < self.conf.parameters.length; ii++) (function (i) {
         var par = self.conf.parameters[i];
 
         if (par.name != name) {
@@ -198,7 +198,7 @@ TsDashboard.prototype.initParams = function () {
                 self.driver.getParamValues(par.name, val, function (options) {
                     $("#opt" + par.name).empty();
                     $("#opt" + par.name).show();
-                    for (var ii in options) (function (i) {
+                    for (var iii = 0; iii < options.length; iii++) (function (i) {
                         var option = $(document.createElement("div"));
                         option.text(options[i]);
                         option.click(function () {
@@ -207,7 +207,7 @@ TsDashboard.prototype.initParams = function () {
                             $("#opt" + par.name).hide();
                         })
                         $("#opt" + par.name).append(option);
-                    })(ii);
+                    })(iii);
                 });
             });
             if (par.default) {
@@ -217,7 +217,7 @@ TsDashboard.prototype.initParams = function () {
         } else if (par.type === "enum") {
             label.append("<select id='in" + par.name + "'></select >");
             self.driver.getParamValues(par.name, null, function (options) {
-                for (var i in options) {
+                for (var i = 0; i < options.length; i++) {
                     var option = options[i];
                     $("#in" + par.name).append("<option value='" + option.value + "'>" + option.caption + "</option>");
                 }
@@ -1232,95 +1232,4 @@ TsDashboardDummyDriver.prototype.getViewDefinition = function (callback) {
 }
 TsDashboardDummyDriver.prototype.getDrawData = function (options, callback) {
     callback(this.data);
-}
-
-/////////////////////////////////////////////////////////////////////////////////////////////
-// Polyfills
-/////////////////////////////////////////////////////////////////////////////////////////////
-
-// Production steps of ECMA-262, Edition 5, 15.4.4.18
-// Reference: http://es5.github.io/#x15.4.4.18
-if (!Array.prototype.forEach) {
-    Array.prototype.forEach = function (callback, thisArg) {
-        var T, k;
-        if (this == null) {
-            throw new TypeError(' this is null or not defined');
-        }
-        // 1. Let O be the result of calling ToObject passing the |this| value
-        // as the argument.
-        var O = Object(this);
-        // 2. Let lenValue be the result of calling the Get internal method of O
-        // with the argument "length".
-        // 3. Let len be ToUint32(lenValue).
-        var len = O.length >>> 0;
-        // 4. If IsCallable(callback) is false, throw a TypeError exception.
-        // See: http://es5.github.com/#x9.11
-        if (typeof callback !== "function") {
-            throw new TypeError(callback + ' is not a function');
-        }
-        // 5. If thisArg was supplied, let T be thisArg; else let T be
-        // undefined.
-        if (arguments.length > 1) {
-            T = thisArg;
-        }
-        // 6. Let k be 0
-        k = 0;
-        // 7. Repeat, while k < len
-        while (k < len) {
-            var kValue;
-            // a. Let Pk be ToString(k).
-            // This is implicit for LHS operands of the in operator
-            // b. Let kPresent be the result of calling the HasProperty internal
-            // method of O with argument Pk.
-            // This step can be combined with c
-            // c. If kPresent is true, then
-            if (k in O) {
-                // i. Let kValue be the result of calling the Get internal
-                // method of O with argument Pk.
-                kValue = O[k];
-                // ii. Call the Call internal method of callback with T as the
-                // this value and
-                // argument list containing kValue, k, and O.
-                callback.call(T, kValue, k, O);
-            }
-            // d. Increase k by 1.
-            k++;
-        }
-        // 8. return undefined
-    };
-}
-
-if (!Array.prototype.filter) {
-    Array.prototype.filter = function (fun/* , thisArg */) {
-        'use strict';
-
-        if (this === void 0 || this === null) {
-            throw new TypeError();
-        }
-
-        var t = Object(this);
-        var len = t.length >>> 0;
-        if (typeof fun !== 'function') {
-            throw new TypeError();
-        }
-
-        var res = [];
-        var thisArg = arguments.length >= 2 ? arguments[1] : void 0;
-        for (var i = 0; i < len; i++) {
-            if (i in t) {
-                var val = t[i];
-
-                // NOTE: Technically this should Object.defineProperty at
-                // the next index, as push can be affected by
-                // properties on Object.prototype and Array.prototype.
-                // But that method's new, and collisions should be
-                // rare, so use the more-compatible alternative.
-                if (fun.call(thisArg, val, i, t)) {
-                    res.push(val);
-                }
-            }
-        }
-
-        return res;
-    };
 }
